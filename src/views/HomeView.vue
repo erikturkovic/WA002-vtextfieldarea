bc4b5522248f711fe5fe1a4f612f12c218c1f082
erikturkovic <eturkovic@student.unipu.hr>
2022-03-25 05:07:58 +0100

    Napravljen text area
    Instliral sam vuetify i složil jednostavni text field.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="5">
+          <v-text-field label="Upiši ime" single-line outlined></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
