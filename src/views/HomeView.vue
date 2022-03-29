a9a5ba8a5796da525ff3b1bbe8fc54bb657149f4
erikturkovic <eturkovic@student.unipu.hr>
2022-03-29 20:21:02 +0200

    Dodan button i data table

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,34 @@
           <v-text-field label="Upiši ime" single-line outlined></v-text-field>
         </v-col>
       </v-row>
+      <v-btn rounded color="primary" dark> OK </v-btn>
+      <v-data-table
+        :headers="headers"
+        :items="desserts"
+        :items-per-page="1"
+        class="elevation-1"
+      ></v-data-table>
     </v-container>
   </v-form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: "Ime",
+          align: "start",
+          sortable: false,
+          value: "ime",
+        },
+        { text: "Države", value: "drzave" },
+        { text: "Godine", value: "godine" },
+        { text: "Spol", value: "spol" },
+        { text: "Vjerojatnost", value: "vjerojatnost" },
+      ],
+    };
+  },
+};
+</script>
