f688231cd7c50a1f801ff8005e4f3a25a6a5a39c
erikturkovic <eturkovic@student.unipu.hr>
2022-03-30 19:26:38 +0200

    async mounted
    ne radi, puno errora

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,12 @@
     <v-container>
       <v-row>
         <v-col cols="12" sm="5">
-          <v-text-field label="Upiši ime" single-line outlined></v-text-field>
+          <v-text-field
+            v-model="name"
+            label="Upiši ime"
+            single-line
+            outlined
+          ></v-text-field>
         </v-col>
       </v-row>
       <v-btn rounded color="primary" dark> OK </v-btn>
@@ -21,19 +26,41 @@
 export default {
   data() {
     return {
+      nation: [],
+      age: [],
+      gender: [],
+      name: [],
+
       headers: [
         {
           text: "Ime",
           align: "start",
           sortable: false,
-          value: "ime",
+          value: "",
         },
         { text: "Države", value: "drzave" },
         { text: "Godine", value: "godine" },
         { text: "Spol", value: "spol" },
-        { text: "Vjerojatnost", value: "vjerojatnost" },
       ],
     };
+  },
+
+  async mounted() {
+    let fecnatio = await fetch("https://api.nationalize.io/?name=" + this.name);
+    let fecage = await fetch("https://api.agify.io/?name=" + this.name);
+    let fecgender = await fetch("https://api.genderize.io/?name=" + this.name);
+
+    let thenation = await fecnatio.json();
+    let theage = await fecage.json();
+    let thegender = await fecgender.json();
+
+    console.log(nation);
+    console.log(age);
+    console.log(gender);
+
+    this.nation = thenation;
+    this.age = theage;
+    this.gender = thegender;
   },
 };
 </script>
